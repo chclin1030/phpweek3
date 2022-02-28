72a4a30795f679800e567f89215c6ea8f128f7de
林嘉琪 <chclin@mail.yihder.com.tw>
2022-02-28 19:05:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# phpWeek1
---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+</head>
+<body>
+
+<?php
+    $i=10;
+    $j=20;
+
+    echo "<h2>Hello!<h2><b>I'm +7!</b>";
+    echo "</br>";
+    echo $i+$j;
+    echo "</br>";
+    echo date("Y,m,d");
+?>
+
+</body>
+</html>
